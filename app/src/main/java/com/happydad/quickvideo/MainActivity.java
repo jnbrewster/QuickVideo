9fa329c1ca38a45edb21958ff1c3f7c3358593c0
joel <j.n.brewster@gmail.com>
2015-11-01 14:22:56 +0900

    Updating to new Android Studio

--- app/src/main/java/com/happydad/quickvideo/MainActivity.java
+++ app/src/main/java/com/happydad/quickvideo/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //TODO: Hide the action bar?
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instantCamera();
@@ -35,6 +32,7 @@ public class MainActivity extends ActionBarActivity {
     public void instantCamera() {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         startActivityForResult(takeVideoIntent, ACTION_TAKE_VIDEO);
+        //release() DataBuffer object
 
     }
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -43,15 +41,7 @@ public class MainActivity extends ActionBarActivity {
 
             if (requestCode == ACTION_TAKE_VIDEO) {
 
-                //Uri videoUri = data.getData();
-                //String filePath = videoUri.getPath();
-                //Log.d("LOGCAT", "Video path is: " + filePath);
-
-                //Toast.makeText(getApplicationContext(), "The video has been saved to your default camera directory. " + filePath, Toast.LENGTH_LONG).show();
-
-                //TODO: test if video always saves in DCIM/Camera directory on different builds.
                 Toast.makeText(getApplicationContext(), "The video has been saved to your default camera directory.", Toast.LENGTH_LONG).show();
-
             }
         }
 
